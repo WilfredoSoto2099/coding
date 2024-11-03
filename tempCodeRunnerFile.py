2ba85eb8f821752c294fe544112caaf4a11a972d
WilfredoSoto2099 <Wilfredosoto2099@gmail.com>
2024-11-03 17:28:53 -0500

    Assignment 2-2

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+rint("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Enter choice
---
+++ test2.py
@@ -0,0 +1,10 @@
+#User Number
+Num1 = float(input('Enter a number:'))
+#Times Number by 10
+result = Num1 * 10
+#Print result 
+print(result)
+
+
+
+
