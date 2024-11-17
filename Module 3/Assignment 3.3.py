0f48656dea6fe21c3c76dabb8667e738231969aa
WilfredoSoto2099 <Wilfredosoto2099@gmail.com>
2024-11-17 18:13:44 -0500

    Assignment 3.3

---
+++ Module 3/Assignment 3.3.py
@@ -0,0 +1,31 @@
+# Function:     This program determines if a date entered by the user is valid.  
+# Input:        Interactive
+month = int(input('Please insert a month: '))
+day = int(input('Please insert a day: '))
+year = int(input('Please insert a year: '))
+# Output:       Valid date is printed or user is alerted that an invalid date was entered.
+validDate = True
+MIN_YEAR = 0
+MIN_MONTH = 1
+MAX_MONTH = 12
+MIN_DAY = 1
+MAX_DAY = 31
+
+
+#validating input
+if year <= MIN_YEAR: # invalid year
+    validDate = False
+elif month < MIN_MONTH or int(month) > MAX_MONTH: # invalid month
+    validDate = False
+elif day < MIN_DAY or int(day) > MAX_DAY: # invalid day
+    validDate = False
+
+# Test to see if date is valid and output date and whether it is valid or not
+
+# endOfJob()
+if validDate == True:
+    # Output statement
+    print(f'{month}/{day}/{year} is a valid date')
+else:
+    # Output statement
+    print (f'{month}/{day}/{year} Is an invalid date')
