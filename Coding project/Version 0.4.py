d203963bede1fe22ac53f010be8abe801c85dd36
WilfredoSoto2099 <Wilfredosoto2099@gmail.com>
2024-11-29 12:28:30 -0500

    version 0.4

---
+++ Coding project/Version 0.4.py
@@ -0,0 +1,105 @@
+import time
+
+def display_header():
+    print()
+    header = '********************************\n'
+    header += 'AutoCountry Vehicle Finder 0.1\n'
+    header += "********************************\n"
+    header += 'Please Enter the following number below from the following menu\n'
+    header += '\n'
+    header += '1. Print all Authorized Vehicles\n'
+    header += '2. Check if a Vehicle is Authorized\n'
+    header += '3. Add a Vehicle to the Authorized List\n'
+    header += '4. Remove a Vehicle from the Authorized list\n'
+    header += '5. Exit\n'
+    time.sleep(1)
+    print(header)
+
+authorized_vehicles = [
+    "Ford F-150",
+    "Chevrolet Silverado",
+    "Tesla CyberTruck",
+    "Toyota Tundra",
+    "Nissan Titan"
+]
+
+def handle_user_choice():
+    while True:
+        display_header()
+        try:
+            time.sleep(1) # readability
+            choice = int(input('Enter your choice (1, 2, 3, 4, or 5): ')) # user input
+            # first choice
+            if choice == 1: 
+                print() # readability
+                print('Printing all Authorized Vehicles...') # readability/answer
+                time.sleep(2) # readability
+                for vehicle in authorized_vehicles:
+                    print()  # Adds a new line for better readability
+                    time.sleep(1) # readability
+                    print(vehicle) # print dictionary
+                print() # readability
+            # second choice
+            elif choice == 2:
+                user_vehicle = input('Please Enter the full vehicle name: ').strip().lower() # user input
+                if user_vehicle in [vehicle.lower() for vehicle in authorized_vehicles]:
+                    time.sleep(1) # readability
+                    print(f'{user_vehicle} is an authorized vehicle') # answer
+                else:
+                    time.sleep(1) # readability
+                    print(f'{user_vehicle} is not an authorized vehicle. If you received this in error, please check the spelling and try again...') # answer
+                print() # readability
+            # third choice
+            elif choice == 3:
+                add_vehicle = input('Please insert the vehicle name you would like to add: ').strip() # user input
+                authorized_vehicles.append(add_vehicle) # add user input to dictionary 
+                time.sleep(1) # readability
+                print('Loading...') # readability/answer
+                time.sleep(3) # readability
+                print(f'{add_vehicle} has been added to the Authorized Vehicle list.') # answer
+                print() # readability
+            # fourth choice
+            elif choice == 4:
+                remove_vehicle = input('Please insert the vehicle name you would like to remove: ').strip()
+                if remove_vehicle in authorized_vehicles:
+                    authorized_vehicles.remove(remove_vehicle) # remove user input from dictionary
+                    time.sleep(1)
+                    print('Loading...')
+                    time.sleep(3)
+                    print(f'{remove_vehicle} has been removed from the Authorized Vehicle list.')
+                    print()
+                else:
+                    print(f'{remove_vehicle} was not found in the list of authorized vehicles.')
+                    print()
+                    time.sleep(2)
+            # fifth choice
+            elif choice == 5:
+                time.sleep(1)
+                print()
+                print('Thank you for using the AutoCountry Vehicle Finder, good-bye!') # end 
+                print()
+                break  # Exit the loop and end the program
+            else:
+                print('Invalid choice. Please enter 1, 2, 3, or 4.') # answer
+                print() # readability
+            time.sleep(2) # readability
+            continue_choice = input('Would you like to continue? (yes/no): ').strip().lower() # user input
+            if continue_choice == 'yes': # no case sensitive
+                print('Loading...') # answer
+                print()
+                print()
+                time.sleep(3) # readability
+                continue  # Restart the loop
+            else:
+                time.sleep(1) # readability
+                print('Thank you for using the AutoCountry Vehicle Finder, good-bye!') # end 
+                print()
+                print()
+                break  # Exit the loop and end the program
+        except ValueError:
+            time.sleep(1)
+            print('Invalid input. Please enter a valid number.')
+            print()
+
+# Start the user choice handling loop
+handle_user_choice()
